ede4b650cdfd1dac77ceb9d5be490bb85403b000
Bere <bre_kc073@hotmail.com>
2022-07-01 23:46:39 -0500

    add first_server folder

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const http = require("http")
 
-const server = http.createServer((request, response)=>{
-    // Escribiendo
-    response.write("Hola desde mi servidor")
+const server = http.createServer((request, response) => {
+  // Destructurar
+  const { url, method } = request
+  console.log("url", url)
+  console.log("method", method)
 
-    //Acabar
-    response.end() //-> es como el return de los servidores
+  // Put -> Reemplazo
+  // Patch -> Actualiza
+
+  // Estructura de datos
+  const endpoints = {
+    GET: {
+      "/hola":"get en /hola",
+    },
+    POST: {
+      "/hola": "POST en /hola, aqui podemos crear",
+    },
+    PATCH: {
+      "/hola": "PATCH en /hola, aqui podemos actualizar",
+    },
+    DELETE:{
+      "/hola": "DELETE en /hola, aqui podemos eliminar",
+      "/adios": "DELETE en /adios, aqui podemos eliminar"
+    } 
+  }
+
+  response.write(endpoints[method][url])
+
+  // Endpoints
+  // Punto final donde llega el request
+  // Se conforma de 2 cosas
+  // 1 - Method -> GET, PUT, PATCH, DELETE
+  // 2 - Ruta -> /home, /koders 
+
+  // Acabar
+  response.end() // -> como el return de los servidores
 })
 
-// Esta escuchando el servidor
-// el primer parametro es el puerto
-// el segundo es una callback anonima
-server.listen(8080, ()=>{
-    console.log("Mi server esta escuchando")
+// Listening
+// 1- PORT
+// 2- CALLBACK
+server.listen(8080, () => {
+  console.log("Mi servidor esta escuchando")
 })
